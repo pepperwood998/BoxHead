384bb9398cad8c7bda5b44fdb6ddf79fbc7bda10
pepperwood998 <tuandt66742@gmail.com>
2022-02-18 21:06:54 +0700

    Add boundary.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
   [SerializeField] float speed = 15.0f;
+  [SerializeField] GameObject boundary;
 
   private Rigidbody2D _rb2d;
   private Vector2 _direction;
+  private float _minX;
+  private float _maxX;
+  private float _minY;
+  private float _maxY;
 
   void Start()
   {
     _rb2d = GetComponent<Rigidbody2D>();
+    UpdateBoundaryCoords(boundary);
   }
 
   void Update()
@@ -31,5 +37,19 @@ public class PlayerMovement : MonoBehaviour
   void FixedUpdate()
   {
     _rb2d.velocity = speed * _direction;
+    transform.position = new Vector2(Mathf.Clamp(transform.position.x, _minX, _maxX), Mathf.Clamp(transform.position.y, _minY, _maxY));
+  }
+
+  private void UpdateBoundaryCoords(GameObject boundary)
+  {
+    Bounds playerSize = transform.Find("Body").GetComponent<SpriteRenderer>().bounds;
+    float playerWidth = playerSize.size.x;
+    float playerHeight = playerSize.size.y;
+
+    Bounds boundarySize = boundary.GetComponent<SpriteRenderer>().bounds;
+    _minX = (boundary.transform.position.x - boundarySize.size.x / 2) + playerWidth / 2;
+    _maxX = (boundary.transform.position.x + boundarySize.size.x / 2) - playerWidth / 2;
+    _minY = (boundary.transform.position.y - boundarySize.size.y / 2) + playerHeight / 2;
+    _maxY = (boundary.transform.position.y + boundarySize.size.y / 2) - playerHeight / 2;
   }
 }
